69b7507aff2dae64c73493f68b21c98411b16743
jgalloni <jgalloni91@gmail.com>
2015-12-01 11:58:15 -0300

    Update copy_ram_to_sd.asm
    "ponele q anda"

--- TPCodigo/TPLDM328/TPLDM328/copy_ram_to_sd.asm
+++ TPCodigo/TPLDM328/TPLDM328/copy_ram_to_sd.asm
@@ -1,77 +1,5 @@
-/*
- * copy_ram_to_sd.asm
- *
- *  Contiene el archivo para guardar la informacion en la SD mediante el protocolo serie
- */ 
-
-COPY_FROM_TO:
-
-
-SPI_MasterInit:
-	; Set MOSI and SCK output, all others input
-	; (1<<DD_MOSI)|(1<<DD_SCK) esta sentencia lque hace es realizar
-	; el corrimiento de los bits al que se va a copiar
-	; DD_MOSI = PB3 es el registro del que estamos copiando los datos
-	; DDR_SPI = DDRB
-	; DD_MISO = PB4
-	; DD_SCK = PB5
-	; SS = 
-
-	LDI R17,(1<<PB3)|(1<<PB5) 
-	OUT DDRB,R17
-	; Enable SPI, Master, set clock rate fck/16
-	LDI R17,(1<<SPE)|(1<<MSTR)|(1<<SPR0)
-	OUT SPCR,r17
-	RET
-
-SPI_MasterTransmit:
-	; Start transmission of data (r16)
-	OUT SPDR,R16
-
-Wait_Transmit:
-	; Wait for transmission complete
-	IN R16, SPSR
-	SBRS R16, SPIF
-	RJMP Wait_Transmitiendo
-	RET
-
-Wait_Transmitiendo:
-	; Wait for transmission complete
-	IN r16, SPSR
-	SBRS r16, SPIF
-	RJMP Wait_Transmit
-	RET
-
-;copy_ram_to_sd.asm
-
-;Archivo que contiene las funciones para realizar la escritura 
-;y lectura de una memoria SD
-
-;A) Inicializar SD
-;******************
-; Pasos para inicializar una tarjeta SD
-; 1)Se debe activar la señal del CLK en 80 ciclos, mientras se
-; tiene el conector ~SS en estado alto. Esto último se logra
-; poniendo el bit ~SS en 0 del microcontrolador.
-; 2) Enviar el comando 0
-; 3) Enviar el comando 1 varias veces (no está predefinido).
-
-;Envio de comando para inicializar tarjeta: "cmd0"
-
-;Comandos relevantes
-;*******************
-;Comando| 	Argumento 	| Respuesta  |  Acción
-;-------|-----------------------|------------|----------------------------
-;CMD0	| [31:0] 0  	   	|  R1 	     | Reinicia la SD
-;CMD1 	| [31:0] 0   	   	|  R1 	     | Confirmación de inicio
-;CMD13  | [31:0] 0   	   	|  R2 	     | pide registro de estado
-;CMD16  | [31:0] largo    	|  R1 	     | Selecciona largo de bloque
-;CMD17  | [31:0] dirección 	|  R1 	     | Leer un bloque
-;CMD24  | [31:0] dilección 	|  R1 	     | Escribir un bloque
-;CMD59  | [31:1] 0 [0:0] CRC op.|  R1 	     | Usar CRC o no
-
-
-hi:	.db	"SD card test",0,0
+	.cseg
+hi:		.db	"SD card test",0,0
 hi2:	.db	"Init successful",0
 hi3:	.db	"Halfway",0
 err1:	.db	"timeout 1",0
@@ -80,45 +8,24 @@ err3:	.db	"too many cmd1",0
 cmd0:	.db	0x40,0x00,0x00,0x00,0x00,0x95	; SD card cmd 0
 cmd1:	.db	0x41,0x00,0x00,0x00,0x00,0xff	; SD card cmd 1
 cmd9:	.db	0x49,0x00,0x00,0x00,0x00,0xff	; SD card cmd 9
+cmdx:   .db 0x50, 0x00,0x00,0x02,0x00, 0xFF,
 
 
-copy_ram_to_sd:
-
-main:	
-	;ldi	r16,high(RAMEND)	; set up stack pointer
-	;out	SPH,r16
-	;ldi	r16,low(RAMEND)
-	;out	SPL,r16
-
-	ldi	r16,0x80		; enable the clock prescaler
-	sts	CLKPR,r16		; for 4 cycles
-	clr	r16
-	sts	CLKPR,r16		; and div factor to 1
-
+SD_INITIALIZE:
 	ldi	ZL,low(hi<<1)		; greetings, earthling
 	ldi	ZH,high(hi<<1)
-	rcall	flashout
-
+	CALL SENDDATA
 	rcall	SPI_Init		; do SPI initialization
 	ldi	ZL,low(hi2<<1)		; greetings again
 	ldi	ZH,high(hi2<<1)
-	rcall	flashout
+	CALL SENDDATA
 
-loop:	rcall	getchar			; see if there's a character coming in
-	breq	loop			; if zero set, no character
-
-	rcall	sendchar		; otherwise, echo it back to them
-	rcall	sendCRLF		; follow it with a CRLF
-	mov	r0,r1
-	rcall	match_jump1
-	.dw	'c',get_csd1
-	.dw	0,0
+	RET
 
 get_csd1:
-	cbi	portb,portb4		; select ~CS
+	cbi	portb,portb1		; select ~CS
 	ldi	zh,high(cmd9<<1)	; send cmd9
 	ldi	zl,low(cmd9<<1)
-	rcall	send_fcmd
 
 	ldi	r16,10			; give them 10 tries
 okw2:	ser	r20			; wait for response
@@ -128,7 +35,7 @@ okw2:	ser	r20			; wait for response
 	cpi	r20,0xff
 	breq	okw2
 	tst	r20
-	brne	timeout2a
+	brne	timeout1a
 
 	ldi	r16,10			; give them 10 tries
 okw3:	ser	r20			; wait for response
@@ -141,8 +48,8 @@ okw3:	ser	r20			; wait for response
 	cpi	r20,0xfe		; block start token
 	brne	timeout1a
 
-	ldi	yl,low(buf)		; read the CSD data
-	ldi	yh,high(buf)
+	ldi	yl,low(TAMANIO_TABLA_TEMPERATURA)		; read the CSD data
+	ldi	yh,high(TAMANIO_TABLA_TEMPERATURA)
 	ldi	r16,18			; 16 bytes & 16 bit CRC
 
 okw4:	ser	r20
@@ -151,31 +58,34 @@ okw4:	ser	r20
 	dec	r16
 	brne	okw4
 
-	sbi	portb,portb4		; deselect SD card
+	sbi	portb,portb1		; deselect SD card
 	ser	r20
 	rcall	SPI_comm		; extra clocks
-	rcall	send_16
-	rcall	sendCRLF
-	rjmp	loop
+;	rcall	send_16
+;	rcall	sendCRLF
+;	rjmp	loop
+	ret
 
 timeout2a:
-	rjmp	timeout2
+	ldi	ZL,low(err2<<1)		; greetings again
+	ldi	ZH,high(err2<<1)
+	CALL SENDDATA
+	ret
 timeout1a:
-	rjmp	timeout1
-
+	ldi	ZL,low(err1<<1)		; greetings again
+	ldi	ZH,high(err1<<1)
+	CALL SENDDATA
+	ret
 
 ; ------------------------------------------------------
-;	SPI_init - initialize SPI communication
-;
-;	this sets up the SPI stuff and initializes the SD card
-;	note: this is hard coded for the AVR Butterfly & SD card test
+;	SPI_init - initializa SPI 
 ;
 ;	  PB1 - SPI clock (pin 5 of SD, SCK)
 ;	  PB2 - Data in (pin 2 of SD, SPI MOSI)
 ;	  PB3 - Data out (pin 7 of SD, SPI MISO)
-;	  PB4 - Chip Sel (pin 1 of SD, SPI ~SS) - note: center joystick!
+;	  PB4 - Chip Sel (pin 1 of SD, SPI ~SS) 
 ;
-;	initialization (may or may not be correct)
+;	inicializacion (may or may not be correct)
 ;	  wait > 1 ms
 ;	  send 10 0xFF with CS high
 ;	  send CMD0 (hex: 40 00 00 00 00 95) with CS low
@@ -190,15 +100,18 @@ SPI_init:
 	push	zh
 	push	zl
 
-	sbi	ddrb,portb4		; PB4 is an output to SD card (~CS)
-	sbi	portb,portb4		; active low, so set high
+	sbi	ddrb,portb2		; PB1 is an output to SD card (~CS)
+	sbi	portb,portb2
+
+	sbi	ddrb,portb1		; PB1 is an output to SD card (~CS)
+	sbi	portb,portb1		; active low, so set high
 	sbi	ddrb,portb0		; dataflash chip select
 	sbi	portb,portb0		; kill the dataflash
 
-	sbi	ddrb,portb1		; B1 is an output (SCK)
-	sbi	ddrb,portb2		; B2 is an output (MOSI)
-	cbi	ddrb,portb3		; B3 is an input (MISO)
-	sbi	portb,portb3		; pullup?
+	sbi	ddrb,portb5		; B5 is an output (SCK)
+	sbi	ddrb,portb3		; B3 is an output (MOSI)
+	cbi	ddrb,portb4		; B4 is an input (MISO)
+	sbi	portb,portb4		; pullup?
 
 	ldi	r16,11			; kill a millisecond or so
 	clr	r20
@@ -207,8 +120,6 @@ splp:	dec	r20
 	dec	r16
 	brne	splp
 
-;	ldi	r16,(1<<spi2x)		; SPI double speed
-;	out	spsr,r16
 ;	enable SPI, master, mode 0
 	ldi	r16,(1<<spe)|(1<<mstr)|(0<<cpha)|(0<<cpol)
 	out	spcr,r16
@@ -216,7 +127,7 @@ splp:	dec	r20
 ;	---------------------- send 74 clock pulses to start it off
 
 	ldi	r16,10			; send 80 clock pulses
-	sbi	portb,portb4		; write 1 to ~CS to deselect SD card
+	sbi	portb,portb1		; write 1 to ~CS to deselect SD card
 
 splp1:	ser	r20			; send 1's
 	rcall	SPI_comm		; clock 8 times each pass
@@ -225,7 +136,7 @@ splp1:	ser	r20			; send 1's
 
 ;	--------------------- send CMD0 to get into SPI mode
 
-	cbi	portb,portb4		; select ~CS
+	cbi	portb,portb1		; select ~CS
 	ldi	zh,high(cmd0<<1)	; send cmd0
 	ldi	zl,low(cmd0<<1)
 	rcall	send_fcmd
@@ -238,7 +149,7 @@ okw:	ser	r20			; wait for 0x01 (idle)
 	cpi	r20,1
 	brne	okw
 
-	sbi	portb,portb4		; deselect SD card
+	sbi	portb,portb1		; deselect SD card
 	ser	r20
 	rcall	SPI_comm		; extra clocks
 
@@ -246,7 +157,7 @@ okw:	ser	r20			; wait for 0x01 (idle)
 
 	ldi	yl,0xff
 	ldi	yh,0x0f
-cc1:	cbi	portb,portb4		; select SD card
+cc1:	cbi	portb,portb1		; select SD card
 	ldi	zh,high(cmd1<<1)
 	ldi	zl,low(cmd1<<1)
 	rcall	send_fcmd		; send cmd1
@@ -259,16 +170,22 @@ cc2:	ser	r20
 	cpi	r20,0xff
 	breq	cc2
 
+cc3:
+
+	ldi	zh,high(cmdx<<1)
+	ldi	zl,low(cmdx<<1)
+	rcall	send_fcmd	
+
 	tst	r20
 	breq	okk
 	sbiw	yh:yl,1
 	breq	timeout3
-	sbi	portb,portb4
+	sbi	portb,portb1
 	ser	r20
 	rcall	SPI_comm
 	rjmp	cc1
 
-okk:	sbi	portb,portb4		; deselect SD card
+okk:	sbi	portb,portb1		; deselect SD card
 	ser	r20
 	rcall	SPI_comm		; extra clocks
 
@@ -290,9 +207,9 @@ timeout1:
 timeout2:
 	ldi	zl,low(err2<<1)		; timeout 2
 	ldi	zh,high(err2<<1) 
-here1:	sbi	portb,portb4		; deselect SD card
-	rcall	flashout
-here:	rjmp	here
+here1:	sbi	portb,portb1		; deselect SD card
+	CALL SENDDATA
+here:	reti
 
 ; ------------------------------------------------------
 ;	send_fcmd - send flash command
@@ -377,76 +294,146 @@ hout:	add	r17,r16		; add constant to our number
 	st	Z+,r17		; and store into output string
 	ret
 
-; -----------------------------------------------------------
-; match_jump1 - find byte match and jump in flash list
-;
-;	parameters:
-;	  R0 - value to match
-;
-;	returns:
-;	  none
-;
-;	usage:
-;	  r0 <-- match value
-;	  call match_jump1
-;	  .dw  v0,addr0     ; jump to addr0 if r0 = v0
-;	  .dw  v1,addr1     ; jump to addr1 if r0 = v1
-;	       ...
-;	  .dw  0,0	    ; marks end of list
-;
-;	note: if there is no match, it jumps to addr0
-;	      therefore, if there are no addresses, this does a reset
+SPI_SEND_BLOCK:
+	;;manda el comando
+	cbi	portb,portb1	
+	LDI R20,0x58
+	call SPI_comm
 
-match_jump1:
-	push	YL
-	push	YH
-	push	ZL
-	push	ZH
-	push	r24
-	push	r25
-	push	r5
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
 
-	in	YL,SPL		; copy of revised stack pointer
-	in	YH,SPH
-	clr	r5		; flag to store first address we see
+	LDI R20,0xFF
+	call SPI_comm
+	mov r18,r20
+	call putchr
 
-	ldd	ZH,Y+8		; high rtn address (word address, not byte)
-	andi	ZH,0x1f		; manual pg 10 says to mask
-	ldd	ZL,Y+9		; low rtn address
-	lsl	ZL		; mult by 2 for byte address
-	rol	ZH		; needed for lpm instr
+	LDI R20,0xFF
+	call SPI_comm
 
-lloop1:	lpm	r24,Z+		; low byte of match value
-	lpm	r25,Z+		; high match byte, should be zero
-	cp	r24,r0		; is low byte what we want?
-	breq	useme1		; yes - hop to
+	LDI R20,0xFF
+	call SPI_comm
 
-	lpm	r24,Z+		; low target address
-	lpm	r25,Z+		; high target address
-	tst	r5		; is this the first address?
-	brne	regad1		; jump if not
+	LDI R20,0xFE
+	call SPI_comm
 
-	inc	r5		; only do this once
-	std	Y+9,r24		; overwrite our return
-	std	Y+8,r25		; with first address in list
+waitsdwrite:	
+	LDI R20,0xff			; send 1's
+	rcall	SPI_comm		; clock 8 times each pass
+	cpi R20,0
+	brne	waitsdwrite
 
-regad1:	tst	r24		; is jump address = 0?
-	brne	lloop1		; low byte not zero - continue
-	tst	r25
-	brne	lloop1		; high byte not zero - continue
-	rjmp	rback1		; yup - head on out
+	;;;;manda los datos
+	ldi	xh,high(TAMANIO_TABLA_TEMPERATURA)	; send cmd0
+	ldi	xl,low(TAMANIO_TABLA_TEMPERATURA)
+SENDBYTESD:
+	LD R20,X+
+	;mov r18,r20
+	;call putchr
+	cpi  R20,0
+	BREQ ENDSENDBLOCK
+	call SPI_comm
+	rjmp SENDBYTESD
 
-useme1:	lpm	r24,Z+		; low target address
-	std	Y+9,r24		; overwrite orig return
-	lpm	r25,Z+		; high target address
-	std	Y+8,r25		; overwrite orig return
+ENDSENDBLOCK:
+	LDI R20,0xFF
+	call SPI_comm
 
-rback1:	pop	r5
-	pop	r25
-	pop	r24
-	pop	ZH
-	pop	ZL
-	pop	YH
-	pop	YL
-	ret			; go to modified return address
+	LDI R20,0xFF
+	call SPI_comm
 
+	LDI R20,0xFE
+	call SPI_comm
+
+waitspisend:
+	LDI R20,0xFF
+	call SPI_comm
+	cpi r20,0xff
+	breq waitspisend
+
+	Sbi	portb,portb1
+
+ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+SPI_READ_BLOCK:
+	;;manda el comando
+	cbi	portb,portb1
+	LDI R20,0x51
+	call SPI_comm
+
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
+	LDI R20,0x00
+	call SPI_comm
+
+	LDI R20,0xFF
+	call SPI_comm
+
+	LDI R20,0xFF
+	call SPI_comm
+	;;;;espera respuesta
+waitsdread:	
+	LDI R20,0xff			; send 1's
+	rcall	SPI_comm		; clock 8 times each pass
+	cpi R20,0
+	brne	waitsdread
+
+waitsdread1:	
+	LDI R20,0xff			; send 1's
+	rcall	SPI_comm		; clock 8 times each pass
+	cpi R20,0xfe
+	brne	waitsdread1
+
+
+
+	;lee y guarda en la tabla
+	ldi	xh,high(TAMANIO_TABLA_TEMPERATURA)	; send cmd0
+	ldi	xl,low(TAMANIO_TABLA_TEMPERATURA)
+	ldi r16,0xff ;
+waitsdread4:	
+	LDI R20,0xff			; send 1's
+	rcall	SPI_comm		; clock 8 times each pass
+	st x+,R20
+	dec	r16			; counting...
+	brne	waitsdread4
+
+	ldi r19,0xff ;
+waitsdread5:	
+	LDI R20,0xff			; send 1's
+	rcall	SPI_comm		; clock 8 times each pass
+	st x+,R20
+	mov r18,r20
+	call putchr
+	dec	r19			; counting...
+	brne	waitsdread5
+
+	;ldi r16,0 ;cambiar por offset
+waitsdread6:	
+	;LDI R20,0xff			; send 1's
+	;rcall	SPI_comm		; clock 8 times each pass
+	;dec	r16			; counting...
+	;brne	waitsdread6
+
+	LDI R20,0xFF
+	call SPI_comm
+	LDI R20,0xFF
+	call SPI_comm
+
+	lds R20,0
+	st x+,R20
+
+	ldi	xh,high(TAMANIO_TABLA_TEMPERATURA)	; send cmd0
+	ldi	xl,low(TAMANIO_TABLA_TEMPERATURA)
+	call SENDDATAFROMMEM
+	Sbi	portb,portb1
+ret
